09b89a01cb657db7b9aa6f332c2a6c4a03f91b18
camila irigoyen <irigoyen.cami@gmail.com>
2022-08-02 15:24:06 -0300

    agregando fetch

--- javascript/main.js
+++ javascript/main.js
@@ -67,12 +67,7 @@ const productos= [
               let productoAComprar = productos.find((el) => el.id == id);
               carrito.splice(carrito.indexOf(productoAComprar), 1);
               mostrarCarrito()
-            }
-           
-          
-           
-                    
-            
+       
           for (item of productos) {
             
             const htmlProducto = document.createElement("div");
@@ -91,7 +86,7 @@ const productos= [
                         </div>
                       `;
                       seccionProductos.appendChild(htmlProducto);
-          
+          }
             
           }
         
@@ -111,111 +106,16 @@ const productos= [
                 title: 'No hay ningnu articulo para comprar',
                 showConfirmButton: false,
                 timer: 1500
-              })
+              });
             }
           }
-        
+       
+   const elegirProductos= async ()=>  {
+      const busqueda= await fetch ("./data/productos.json");
+      const data = await busqueda.json ();
+     console.log(data);
+    }
+    elegirProductos();
+          
 
 
-// const seccionProductos = document.getElementById("productos");
-
-// for (item of productos) {
-//   const htmlProducto = document.createElement("div");
-//   htmlProducto.classList.add("row");
-
-//   htmlProducto.innerHTML = `
-//               <div class="col-md-12">
-//                 <div class="card">
-//                   <div class="card-body img-fluid">
-//                     <h5 class="card-title">${item.nombre}</h5>
-//                       <p class="card-text">${item.descripcion}</p>
-//                       <button class="btn btn-primary" onclick="agregarCarrito(${item.id})">Comprar $${item.precio}</span>
-//                   </div>
-//                 </div>
-//               </div>
-//             `;
-
-//   seccionProductos.appendChild(htmlProducto);
-// }
-            
-         
-   
-         
-// function elegirPago (arr,filtro) {
-//   let metodoPago = arr.filter ((el) => {switch(filtro) {
-
-//             case "1": 
-//                 console.log("Has seleccionado transferencia bancaria");
-//                 break;
-    
-//                 case "2":
-//                     console.log("Has seleccionado mercado pago");
-//                     break;
-    
-//                     case "3":
-//                         console.log("Has seleccionado cuenta dni");
-//                 break;
-    
-//                 case "4":
-//                     console.log("Has seleccionado tarjeta de crédito o débito");
-//                 break;
-    
-//                 case "5":
-//                     console.log("Has seleccionado cuotas");
-//                     let tarjeta=prompt("Ingresa una opción \n 1-Tarjeta Visa \n 2-Mastercard \n 3-Otra");
-//                     while(opción !=="0"){switch (tarjeta) {
-//                         case "visa":
-//                             console.log("hasta 3 cuotas sin interes");
-//                             break;
-//                             case "mastercard":
-//                                 console.log("una cuota");
-//                                 break;
-//                                 case "otra":
-//                                 console.log("intereés del 5% por cuota");
-//                                 break;}};
-                        
-    
-//                 default:
-    
-//                     console.log  ("Opción no válida");
-//                     break;}
-    
-//                     opción= prompt("Ingresa una opción \n 1-transferencia bancaria \n 2-mercado pago \n 3-cuenta dni \n 4-tarjeta de crédito o débito \n 5-cuotas \n");
-//                     if (opción="0");
-//                    console.log("Esperamos que disfrutes tu compra");});}
-
-
-
-//     const eleccionPago = function elegirPago (metodoPago, filtro): 
-     
-
-
-
-  
-
-
-
-//         function porPrecio (productos,precio){
-//           precio.productos.forEach ((element) => {(console.log (precio*1.20));
-//             return el.precio;})}
-
-//             const productosFiltrados= filtrarProducto(productos,ingreso);
-//           console.log(productosFiltrados);
-
-          //probando acceder a cambiar las etiquetas dentro de js//
-        //   let h3= document.getElementsByTagName("h3");
-        //   console.log(h3);
-
-        //   h3[0].innerHTML= "<h3>Te mostramos en lo que nos destacamos<br><br><p>Hacete un mimo y autoregalate alguno de nuestros productos</p></h3>"
-
-        //   console.dir(document.head)
-
-        //  let h4 = document.getElementById("descuento");
-        //  console.log(h4);
-        
-        
-
-        
-
-                    
-         
